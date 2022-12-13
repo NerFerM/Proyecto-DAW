56a5e6eda4b2ef4c96ecd73394da42a02fa9b918
Nerea <nerea.fernandez@tcomet.com>
2022-12-13 12:20:13 +0100

    modified:   clipdiving-server/dist/index.js

--- clipdiving-server/dist/index.js
+++ clipdiving-server/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const usuario_1 = __importDefault(require("./routes/usuario"));
@@ -15,7 +16,7 @@ const app = express();
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
 server.app.use((0, express_fileupload_1.default)({ useTempFiles: true }));
-// server.app.use(cors({origin: true, credentials: true}));
+server.app.use((0, cors_1.default)({ origin: true, credentials: true }));
 server.app.use('/user', usuario_1.default);
 server.app.use('/posts', post_1.default);
 mongoose_1.default.connect('mongodb+srv://NerFerM:W8zlvLodnZDPk8lk@clipdiving.c2i7ngo.mongodb.net/clipdiving?retryWrites=true&w=majority', (err) => {
